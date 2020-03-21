a11df46e44140c425b890f3a6e98bc4b8b8bd912
FilipRegenczuk <f.regenczuk@outlook.com>
2020-03-21 18:02:19 +0100

    adding blurr effect

--- motion_detector.py
+++ motion_detector.py
@@ -1,13 +1,25 @@
 import cv2
 
+first_frame = None
+
 video = cv2.VideoCapture(0)
 
 while True:
     check, frame = video.read()
-    print(check)
-    print(frame)
 
-    cv2.imshow("Capturing", frame)
+    # frame color edits
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurred_gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+    # set first frame to gray
+    if first_frame is None:
+        first_frame = gray
+        continue
+    
+    # comparing difference
+    delta_frame = cv2.absdiff(first_frame, gray)
+
+    cv2.imshow("Capturing", delta_frame)
     key = cv2.waitKey(1)
 
     if key == ord('q'):

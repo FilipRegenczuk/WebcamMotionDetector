a0d22178d91252c5524a9125687a2e9ba09067d1
FilipRegenczuk <f.regenczuk@outlook.com>
2020-03-21 18:32:50 +0100

    add findContours and drawing rectangle

--- motion_detector.py
+++ motion_detector.py
@@ -16,14 +16,26 @@ while True:
         first_frame = gray
         continue
     
-    # comparing difference
-    delta_frame = cv2.absdiff(first_frame, gray)
 
-    tresh_delta_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    delta_frame = cv2.absdiff(first_frame, gray)                                # comparing difference
+    tresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]     # adding treshold effect
+    tresh_frame = cv2.dilate(tresh_frame, None, iterations=2)                   # clearing frame
+
+
+    (cnts,_) = cv2.findContours(tresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in cnts:
+        if cv2.contourArea(contour) < 1000:
+            continue
+
+        (x, y, w, h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0,0,255), 2)
+
 
     cv2.imshow("Gray Frame", gray)
     cv2.imshow("Delta Frame", delta_frame)
-    cv2.imshow("Treshold Frame", tresh_delta_frame)
+    cv2.imshow("Treshold Frame", tresh_frame)
+    cv2.imshow("Color Frame", frame)
     key = cv2.waitKey(1)
 
     if key == ord('q'):
